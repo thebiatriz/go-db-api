075978f536212e98447c6574731c93d3e0470bfb
Beatriz Monteiro <biafigmonteiro@gmail.com>
2025-10-27 15:09:43 -0300

    (refactor) Routing setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,58 +3,15 @@ package main
 import (
 	"log"
 
-	"github.com/gin-gonic/gin"
-	"github.com/thebiatriz/go-db-api/internal/auth"
-	"github.com/thebiatriz/go-db-api/internal/database"
-	"github.com/thebiatriz/go-db-api/internal/handlers"
-	"github.com/thebiatriz/go-db-api/internal/repositories"
-	"github.com/thebiatriz/go-db-api/internal/usecases"
+	"github.com/thebiatriz/go-db-api/internal/server"
 )
 
 func main() {
-	router := gin.Default()
-
-	dbConnection, err := database.ConnectDB()
+	server, err := server.NewServer()
 
 	if err != nil {
-		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
+		log.Fatalf("Erro ao inicializar o servidor: %v", err)
 	}
 
-	jwtService, err := auth.NewJWTService()
-
-	if err != nil {
-		log.Fatalf("Erro ao inicializar o serviço do JWT: %v", err)
-	}
-
-	ProductRepository := repositories.NewProductRepository(dbConnection)
-	ProductUseCase := usecases.NewProductUsecase(ProductRepository)
-	ProductHandler := handlers.NewProductHandler(ProductUseCase)
-
-	UserRepository := repositories.NewUserRepository(dbConnection)
-	UserUsecase := usecases.NewUserUsecase(UserRepository, jwtService)
-	UserHandler := handlers.NewUserHandler(UserUsecase)
-
-	router.POST("/users", UserHandler.CreateUser)
-	router.POST("/login", UserHandler.Login)
-
-	protected := router.Group("/")
-	protected.Use(handlers.AuthMiddleware(jwtService, UserRepository))
-
-	productRoutes := protected.Group("/products")
-	productRoutes.GET("/", ProductHandler.GetProducts)
-	productRoutes.GET("/:id", ProductHandler.GetProductById)
-	productRoutes.POST("/", ProductHandler.CreateProduct)
-	productRoutes.DELETE("/:id", ProductHandler.DeleteProduct)
-	productRoutes.PUT("/:id", ProductHandler.UpdateProduct)
-
-	userRoutes := protected.Group("/users")
-	userRoutes.GET("/me", UserHandler.GetMe)
-	userRoutes.GET("/", UserHandler.GetUsers)
-	userRoutes.GET("/:id", UserHandler.GetUserById)
-	userRoutes.DELETE("/me", UserHandler.DeleteMe)
-	userRoutes.DELETE("/:id", UserHandler.DeleteUser)
-	userRoutes.PUT("/me", UserHandler.UpdateMe)
-	userRoutes.PUT("/:id", UserHandler.UpdateUser)
-
-	router.Run(":8080")
+	server.Run(":8080")
 }
